98537e79bc8dc434d56c2c5fdfe6f63bbbfd3121
Dialest <dialest@hotmail.com>
2020-02-09 20:42:18 -0800

    another attempt at https

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require('path');
+// const sslRedirect = require('heroku-ssl-redirect');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.use( (req, res) => {
   if(!req.secure) {
