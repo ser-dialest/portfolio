38b1a2ed3bfddec894492e54458b33b269d08926
Dialest <dialest@hotmail.com>
2020-02-09 21:00:46 -0800

    trying ssl-redirect npm

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const express = require("express");
-const path = require('path');
-// const sslRedirect = require('heroku-ssl-redirect');
+const path = require("path");
+const sslRedirect = require('heroku-ssl-redirect');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
+app.use(sslRedirect());
+app.use(express.static("public"));
 
-app.use( (req, res) => {
-  if(req.header('x-forwarded-proto') !== "https") {
-    console.log("redirecting to https");
-    res.redirect("https://" + req.headers.host + req.url);
-  }
-})
+// app.use( (req, res) => {
+
+//   if(req.header('x-forwarded-proto') !== 'https') {
+//     console.log("redirecting to https");
+//     res.redirect("https://" + req.headers.host + req.url);
+//   }
+// })
+
+// if(process.env.NODE_ENV === 'production') {
+//   app.use((req, res, next) => {
+//     if (req.header('x-forwarded-proto') !== 'https') {
+//       res.redirect(`https://${req.header('host')}${req.url}`)
+//     } else {
+//       next()
+//     }
+//   })
+// }
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
