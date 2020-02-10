b9bc779078722ff88f372651a0b627a84061d1c3
Dialest <dialest@hotmail.com>
2020-02-09 21:03:00 -0800

    remove comments/old code

--- server.js
+++ server.js
@@ -10,24 +10,6 @@ app.use(express.json());
 app.use(sslRedirect());
 app.use(express.static("public"));
 
-// app.use( (req, res) => {
-
-//   if(req.header('x-forwarded-proto') !== 'https') {
-//     console.log("redirecting to https");
-//     res.redirect("https://" + req.headers.host + req.url);
-//   }
-// })
-
-// if(process.env.NODE_ENV === 'production') {
-//   app.use((req, res, next) => {
-//     if (req.header('x-forwarded-proto') !== 'https') {
-//       res.redirect(`https://${req.header('host')}${req.url}`)
-//     } else {
-//       next()
-//     }
-//   })
-// }
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
