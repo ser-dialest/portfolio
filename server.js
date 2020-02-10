d40d8e3c2a0a755dd689f005e5f527605032843b
Dialest <dialest@hotmail.com>
2020-02-09 20:28:17 -0800

    redirect to https

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const path = require('path');
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.use( (req, res) => {
+  if(!req.secure) {
+    console.log("redirecting to https");
+    res.redirect("https://" + req.headers.host + req.url);
+  }
 })
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log("Server is listening on PORT: " + PORT);
